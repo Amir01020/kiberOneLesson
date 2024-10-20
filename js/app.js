1e26a3326419d883225031bf04dcf772830d36ed
Aress <112501385+Amir01020@users.noreply.github.com>
2024-10-20 15:20:02 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
-let obj = {
-    name: 'kiberOne  ', 
-    password: '123 123'
-};
-console.log(obj.name);
-console.log(obj.password);
+let url = 'https://kiberone-150ab-default-rtdb.europe-west1.firebasedatabase.app/users.json';
+
 let nameUser = document.querySelector('#nameUser');
 let passwordUser = document.querySelector('#passwordUser');
 let btn = document.querySelector('#btn');
 
 btn.onclick = () => {
-    if (nameUser.value === obj.name && passwordUser.value === obj.password) {
-        alert('Доступ разрешён');
-    } else {
-        alert('Доступ запрещён');
-        
-    }
+    axios.get(url)
+        .then(res => {
+            // Проверьте точные ключи в ответе
+            if (nameUser.value == res.data.userName && passwordUser.value == res.data.userPassword) {
+                alert('Доступ разрешён');
+            } else {
+                
+            }
+        })
+        .catch(err => {
+            console.error('Ошибка запроса:', err);
+            alert('Произошла ошибка при запросе.');
+        });
 };
