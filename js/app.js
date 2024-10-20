1e345e612e66edb6eed8e339f52bca765bbd8973
Aress <112501385+Amir01020@users.noreply.github.com>
2024-10-20 14:24:46 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let obj = {
+    name: 'alex', 
+    password: 'aress123'
+};
+console.log(obj.name);
+console.log(obj.password);
+let nameUser = document.querySelector('#nameUser');
+let passwordUser = document.querySelector('#passwordUser');
+let btn = document.querySelector('#btn');
+
+btn.onclick = () => {
+    if (nameUser.value === obj.name && passwordUser.value === obj.password) {
+        alert('Доступ разрешён');
+    } else {
+        alert('Доступ запрещён');
+        
+    }
+};
